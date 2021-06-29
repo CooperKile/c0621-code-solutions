3e4eb5d79c89bda5168403818408b97b659a8ec9
CooperKile <Cooperk1807@gmail.com>
2021-06-29 11:28:33 -0700

    Revised Javascript data model.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -7,11 +7,13 @@ var orderHistory = [
     details: '',
     invoice: '',
     delivered: 'Aug 8, 2020',
-    image: '',
-    title: 'Javascript for impatient programmers',
-    author: 'Rauschmayer, Dr. Axel',
-    return: 'Return window closed on Sep 7, 2020',
-    price: '31.55',
+    item: {
+      image: [''],
+      title: ['Javascript for impatient programmers'],
+      author: ['Rauschmayer, Dr. Axel'],
+      return: ['Return window closed on Sep 7, 2020'],
+      price: ['$31.55']
+    },
     again: '',
     view: '',
     review: ''
@@ -25,11 +27,13 @@ var orderHistory = [
     invoice: '',
     delivered: 'Jul 20, 2020',
     status: 'Your package was delivered. It was handed directly to a resident.',
-    image: '',
-    title: 'The Timeless Way of Building',
-    author: 'Alexander, Christopher',
-    return: 'Return window closed on Aug 19, 2020',
-    price: '41.33',
+    item: {
+      image: [''],
+      title: ['The Timeless Way of Building'],
+      author: ['Alexander, Christopher'],
+      return: ['Return window closed on Aug 19, 2020'],
+      price: ['41.33']
+    },
     again: '',
     view: '',
     review: ''
@@ -43,9 +47,12 @@ var orderHistory = [
     invoice: '',
     delivered: 'Jul 7, 2020',
     status: 'Your package was delivered. It was handed directly to a resident',
-    image: '',
-    title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Apater---',
-    return: 'Return window closed on Aug 5, 2020',
+    item: {
+      image: [''],
+      title: ['Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Apater---'],
+      return: ['Return window closed on Aug 5, 2020'],
+      price: ['$15.98']
+    },
     again: '',
     view: '',
     review: ''
@@ -58,9 +65,12 @@ var orderHistory = [
     details: '',
     invoice: '',
     delivered: 'Jul 5, 2020',
-    title: ['GameCube Controller', 'The Art of Sql'],
-    return: 'Return window closed on Aug 4, 2020',
-    price: ['$94.95, 33.99'],
+    item: {
+      images: ['', ''],
+      title: ['GameCube Controller', 'The Art of Sql'],
+      return: ['Return window closed on Aug 4, 2020'],
+      price: ['$94.95, 33.99']
+    },
     again: '',
     view: '',
     support: '',
