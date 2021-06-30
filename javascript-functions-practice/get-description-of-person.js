0d5f47a2e9505240851e8b58cae2dea541e275d4
CooperKile <Cooperk1807@gmail.com>
2021-06-30 11:05:08 -0700

    Completed jAvasctipt functions practice.

--- javascript-functions-practice/get-description-of-person.js
+++ javascript-functions-practice/get-description-of-person.js
@@ -1,5 +1,5 @@
 /* exported getDescriptionOfPerson */
 function getDescriptionOfPerson(person) {
-  var x = person.name + ' ' + 'is a' + ' ' + person.occupation + ' ' + 'from' + ' ' + person.birthPlace;
+  var x = person.name + ' ' + 'is a' + ' ' + person.occupation + ' ' + 'from' + ' ' + person.birthPlace + '.';
   return x;
 }
