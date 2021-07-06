629bdde781e6420ddeb26a907d816f10aa90c272
CooperKile <Cooperk1807@gmail.com>
2021-07-06 15:17:53 -0700

    unfinished string manipulation.

--- string-manipulation-2/src/truncate.js
+++ string-manipulation-2/src/truncate.js
@@ -1,3 +1,9 @@
 /* exported truncate */
 // we will be returning a shortened version of the string
 // the length number determines how many characters will be returned
+function truncate(length, string) {
+  if (string.length <= length) {
+    return string + '...';
+  }
+  return string.slice(string, length) + '...';
+}
