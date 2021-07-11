414c583a54b52f679bd2958d6a6f2448b1f8f7c2
CooperKile <Cooperk1807@gmail.com>
2021-07-10 18:47:47 -0700

    Completed javascript view swapping.

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -5,8 +5,9 @@
 // compare the current position to each of the event targets
 // if the event target is equal to the element update its className
 // after updating the class name make the element active
+// get the value of the data view attribute of the event target and assign it a variable
+// create a loop, loop through the view list elements,
 
-// query the dom for tab container, node list of tab and view elements
 var $tabContainer = document.querySelector('.tab-container');
 var $tabList = document.querySelectorAll('.tab');
 var $viewList = document.querySelectorAll('.view');
@@ -18,6 +19,14 @@ $tabContainer.addEventListener('click', function (event) {
         $tabList[i].className = 'tab active';
       } else {
         $tabList[i].className = 'tab';
+      }
+    }
+    var $viewAttribute = event.target.getAttribute('data-view');
+    for (var j = 0; j < $viewList.length; j++) {
+      if ($viewList[j].getAttribute('data-view') === $viewAttribute) {
+        $viewList[j].className = 'view active';
+      } else {
+        $viewList[j].className = 'view hidden';
       }
     }
   } return $viewList;
