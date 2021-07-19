27471526eb9c11a049f3654ddbbf9c8af0ebb68f
CooperKile <Cooperk1807@gmail.com>
2021-07-19 16:03:15 -0700

    Completed JavaScript Constructors.

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,23 @@
+var newObj = new Object();
+console.log('value of newObj:', newObj);
+console.log('type of newObj:', typeof newObj);
+
+var newArr = new Array();
+console.log('value of newArr:', newArr);
+console.log('typeof newArr:', typeof newArr);
+
+var newFunc = new Function();
+console.log('value of newFunc:', newFunc);
+console.log('typeof newFunc:', typeof newFunc);
+
+var newString = new String();
+console.log('value of newString:', newString);
+console.log('type of newString:', typeof newString);
+
+var newNum = new Number();
+console.log('value of newNum:', newNum);
+console.log('typeof newNum:', typeof newNum);
+
+var boolean = new Boolean();
+console.log('value of boolean:', boolean);
+console.log('typeof boolean', typeof boolean);
