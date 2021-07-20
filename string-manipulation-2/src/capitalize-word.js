11f273d516cbd1a377288f5926de38406dd59b0b
CooperKile <Cooperk1807@gmail.com>
2021-07-20 10:00:35 -0700

    In progress.

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,1 +1,12 @@
 /* exported capitalizeWord */
+
+// start at the first index
+// capitalize the first letter
+// lower case the rest of the word
+// if the word = "JavaScript", capitalize the S
+
+function capitalizeWord(word) {
+  if (word[0] !== word[0].toUpperCase()) {
+    return word;
+  }
+}
