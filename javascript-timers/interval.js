d07e69903b3f10ad58a2a430c69234a3d535d0f3
CooperKile <Cooperk1807@gmail.com>
2021-07-20 17:07:53 -0700

    Revised JavaScript timers.

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,11 +1,12 @@
 setInterval(countdown, 1000);
 var count = 4;
-clearInterval(countdown);
+
 var h1 = document.querySelector('h1');
 function countdown(numbers, stirng) {
   if (count > 0) {
     h1.textContent = count--;
   } else {
     h1.textContent = '~Earth Beeeelooowww Us~';
+    clearInterval(countdown);
   }
 }
