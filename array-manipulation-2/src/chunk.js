d493d9110cf0c6ffa0d5f0a18601287c640c4938
Cooper Kile <cooperk1807@gmail.com>
2021-08-02 05:34:11 +0000

    In progress.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,2 +1,18 @@
 /* exported chunk */
 // function chunk(array, size)
+// function chunk(array, size) {
+//   var newArr = [];
+//   var tempArr = [];
+//   for (var i = 0; i < array.length; i++) {
+//     if (i === 0) {
+//       tempArr.push(array[i]);
+//     } else if () === 0) {
+//       newArr.push(tempArr);
+//       tempArr = [];
+//       tempArr.push(array[i]);
+//     } else if () !== 0) {
+//       newArr.push(array[i]);
+//     }
+//   }
+//   return newArr;
+// }
