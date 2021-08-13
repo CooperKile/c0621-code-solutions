d6fb8d40d1c7cb9cf98a9dbb33006bd389ade821
Cooper Kile <cooperk1807@gmail.com>
2021-08-13 22:49:10 +0000

    Completed express hello world.

---
+++ express-hello-world/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+app.use(function (req, res, next) {
+  res.send('Hello World!');
+  next();
+});
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Express server listening on port 3000');
+});
