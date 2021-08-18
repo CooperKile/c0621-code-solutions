89117acd8d81d4752b8f72846bd95fd493c4bd95
Cooper Kile <cooperk1807@gmail.com>
2021-08-18 00:34:58 +0000

    Completed sql delete.

---
+++ sql-delete/bizarre.sql
@@ -0,0 +1,3 @@
+delete from "cities"
+where "name" = 'Pyongyang';
+-- Delete the city of 'Pyongyang'.
---
+++ sql-delete/puritanical.sql
@@ -0,0 +1,3 @@
+delete from "films"
+where "rating" != 'G';
+-- Delete all films that are not rated 'G'. This should produce an error.
