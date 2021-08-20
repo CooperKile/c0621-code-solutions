9a013b8a4674b98765f35f4d3a49c117c9f7dbf2
Cooper Kile <cooperk1807@gmail.com>
2021-08-20 00:54:51 +0000

    In progress express notes api.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,20 +1,52 @@
 const express = require('express');
 const app = express();
+const notes = require('./data.json');
+
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Express server listening on port 3000');
 });
-const notes = require('./data.json');
-app.get(notes, (req, res) => {
+
+app.get('/api/notes', (req, res) => {
   const allNotes = [];
   for (const key in notes) {
     allNotes.push(notes[key]);
   }
+  res.send(allNotes);
 });
-const fs = require('fs');
-fs.readFile('/api/notes', (req, res) => {
-  const allNotes = [];
-  for (const key in notes) {
-    allNotes.push(notes[key]);
+
+app.get('/api/notes/:id', (req, res) => {
+  const errorObj = {
+    400: {
+      400: 'id must be a positive integer'
+    },
+    404: {
+      404: 'cannot find note with id'
+    }
+  };
+  if (req.params.id < 0) {
+    res.status(400).json(errorObj[400]);
+  } else if (notes.notes[req.params.id]) {
+    res.json(notes.notes[req.params.id]);
+  } else {
+    res.status(404).json(errorObj[404]);
+  }
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const errorObj = {
+    400: {
+      400: 'id must be a positive integer'
+    },
+    404: {
+      404: 'cannot find note with id'
+    }
+  };
+  if (req.params.id < 0) {
+    res.status(400).json(errorObj[400]);
+  } else if (notes.notes[req.params.id]) {
+    delete res.json(notes.notes[req.params.id]);
+  } else {
+    res.status(404).json(errorObj[404]);
   }
 });
